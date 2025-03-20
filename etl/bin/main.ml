31ca691e8f4b7271b91b839399acf435f1facf2b
PedroPertusi <pedrovazpertusi@gmail.com>
2025-03-20 14:16:31 +0000

    feat: etl_extract, allows for request on csv, download and convert to string

--- etl/bin/main.ml
+++ etl/bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+let () =
+  print_endline "Starting ETL pipeline...";
+  let url = "https://raw.githubusercontent.com/PedroPertusi/etl-ocaml/main/etl/data/order.csv" in
+  let csv_data = Lwt_main.run (Etl.Etl_extract.fetch_csv url) in
+  print_endline csv_data
---
+++ etl/lib/etl_extract.ml
@@ -0,0 +1,13 @@
+(* etl_extract.ml *)
+
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+let fetch_csv (url : string) : string Lwt.t =
+  Client.get (Uri.of_string url) >>= fun (resp, body) ->
+  let status = resp |> Response.status |> Code.code_of_status in
+  if status = 200 then
+    Cohttp_lwt.Body.to_string body
+  else
+    Lwt.fail_with (Printf.sprintf "HTTP request failed with code: %d" status)
